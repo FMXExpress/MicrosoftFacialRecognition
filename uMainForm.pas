525e65a32f5829843243424e2e8942a9a8b91bea
FMXExpress <eli@peacekeeper.com>
2021-07-01 23:00:47 -0700

    Initial commit.

---
+++ uMainForm.pas
@@ -0,0 +1,48 @@
+unit uMainForm;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, REST.Types,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Param,
+  FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf, FireDAC.DApt.Intf,
+  System.Rtti, FMX.Grid.Style, FMX.StdCtrls, FMX.Controls.Presentation,
+  FMX.ScrollBox, FMX.Grid, Data.DB, FireDAC.Comp.DataSet, FireDAC.Comp.Client,
+  REST.Response.Adapter, REST.Client, Data.Bind.Components,
+  Data.Bind.ObjectScope, Data.Bind.EngExt, Fmx.Bind.DBEngExt, Fmx.Bind.Grid,
+  System.Bindings.Outputs, Fmx.Bind.Editors, Data.Bind.Grid, Data.Bind.DBScope;
+
+type
+  TMainForm = class(TForm)
+    RESTClient1: TRESTClient;
+    RESTRequest1: TRESTRequest;
+    RESTResponse1: TRESTResponse;
+    RESTResponseDataSetAdapter1: TRESTResponseDataSetAdapter;
+    FDMemTable1: TFDMemTable;
+    StringGrid1: TStringGrid;
+    Button1: TButton;
+    MaterialOxfordBlueSB: TStyleBook;
+    BindSourceDB1: TBindSourceDB;
+    BindingsList1: TBindingsList;
+    LinkGridToDataSourceBindSourceDB1: TLinkGridToDataSource;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.fmx}
+
+procedure TMainForm.Button1Click(Sender: TObject);
+begin
+  RESTRequest1.ExecuteAsync();
+end;
+
+end.
